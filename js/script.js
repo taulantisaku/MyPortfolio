ffa6832e9a136b67a108f4ffa716f01988e75350
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-09-18 17:56:52 +0200

    superslide

--- js/script.js
+++ js/script.js
@@ -7,4 +7,12 @@ $(document).ready(function() {
 			pagination: false
 		});
 
+	var typed = new Typed(".typed", {
+		strings: ["Web Developer.","Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
 })

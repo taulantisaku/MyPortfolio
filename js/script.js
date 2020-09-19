0b6d996fa5211e5b2ca39910cbdf4341d9180967
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-09-19 15:52:55 +0200

    Skill section

--- js/script.js
+++ js/script.js
@@ -17,19 +17,43 @@ $(document).ready(function() {
 
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    margin:10,
-	    nav:true,
+	    items: 4,
 	    responsive:{
 	        0:{
 	            items:1
 	        },
-	        600:{
+	        480:{
+	            items:2
+	        },
+	        768:{
 	            items:3
 	        },
-	        1000:{
-	            items:5
+	        938:{
+	            items:4 
 	        }
 	    }
 	});
+
+
+        var skillsTopOffset = $(".skillsSection").offset().top;
+
+        $(window).scroll(function() {
+
+        	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200)
+
+        		$('.chart').easyPieChart({
+		         	easing: 'easeInOut',
+		         	barColor: '#fff',
+		         	trackColor: false,
+		         	scaleColor: false,
+		         	lineWidth: 4,
+		         	size: 152,
+		         	onStep: function(from,to,percent){
+		         		$(this.el).find('.percent').text(Math.round(percent));
+		         	}
+        });
+
+        });
+
 
 })

98ec2f22a5cf76ab189f30bbf50b1237274a1614
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-09-22 15:19:05 +0200

    Portfolio

--- js/script.js
+++ js/script.js
@@ -81,6 +81,37 @@ $(document).ready(function() {
         	element.countup(endVal);
         })
 
+        $("[data-fancybox]").fancybox();
+
+        $(".items").isotope({
+        	filter: '*',
+        	animationOptions: {
+        		duration: 1500,
+        		easing: 'linear',
+        		queue: false
+        	}
+        })
+
+
+        $("#filters a").click(function(){
+
+        	$("#filters .current").removeClass("current");
+        	$(this).addClass("current");
+
+        	var selector = $(this).attr("data-filter");
+
+	        	$(".items").isotope({
+	        	filter: 'selector',
+	        	animationOptions: {
+	        		duration: 1500,
+	        		easing: 'linear',
+	        		queue: false
+	        	}
+	       });
+
+	       return false;
+
+        });
 
 });
 
